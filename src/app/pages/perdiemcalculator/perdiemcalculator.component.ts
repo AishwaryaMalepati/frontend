4296f61548a566731acee81cec140fec75f88d03
Sachin Kukreja <sachin.kukreja@iiitdmj.ac.in>
2017-09-24 01:36:35 +0530

    Integrated api

--- src/app/pages/perdiemcalculator/perdiemcalculator.component.ts
+++ src/app/pages/perdiemcalculator/perdiemcalculator.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {EventService} from '../../services/event.service';
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-perdiemcalculator',
@@ -6,10 +8,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./perdiemcalculator.component.css']
 })
 export class PerdiemcalculatorComponent implements OnInit {
+  current_period: string;
+  perdiemList: any[]=[];
+  errorMessage: string;
 
-  constructor() { }
+  public constructor(private eventService: EventService, private router: Router) { }
+
+  getCurMonthName(date) {
+    var monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    return monthNames[date.getMonth()];
+  };
 
   ngOnInit() {
+    const today = new Date();
+    const monthLabels = ['Mar-Apr', 'Apr-May', 'May-Jun', 'Jun-Jul', 'Jul-Aug', 'Aug-Sep', 'Sep-Oct', 'Oct-Nov'];
+    var curMonthName = this.getCurMonthName(today);
+
+    if (['Jan', 'Feb'].indexOf(curMonthName) !== -1) {
+      this.current_period = 'Jan-Mar';
+    } else if (curMonthName === 'Nov') {
+      this.current_period = 'Oct-Nov';
+    } else {
+      this.current_period = monthLabels.find(function(el) {
+        return el.startsWith(curMonthName);
+      });
+
+      if (today.getDate() < 15) {
+        const monthIndex = monthLabels.indexOf(this.current_period);
+        this.current_period = monthLabels[monthIndex - 1];
+      }
+    }
   }
 
+  calculatePerdiem() {
+    this.eventService.saveEvent({}, 'perdiem')
+      .subscribe(event => {
+          this.perdiemList = event;
+        },
+        error => {this.errorMessage = <any>error;}
+      );
+  }
 }
